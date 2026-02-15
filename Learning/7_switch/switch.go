447c12c791ba6d45ffa69f6974d9fbfd40164da3
eharshit <harshitgraphy@gmail.com>
2026-02-16 03:44:35 +0530

    switch

---
+++ Learning/7_switch/switch.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	i := 2
+	fmt.Print("write ", i, " as ")
+	switch i{
+	case 1:
+		fmt.Println("one")
+
+	case 2:
+		fmt.Println("two")
+	
+	case 3:
+		fmt.Println("three")
+	}
+	
+	switch time.Now().Weekday(){
+	case time.Saturday, time.Sunday:
+		fmt.Println("Its weekend")
+	default:
+		fmt.Println("Its weekday :(")
+}
+
+	whatAmI := func(i interface{}){
+		switch t := i.(type){
+		case bool:
+			fmt.Println("I'm a bool")
+		case int:
+			fmt.Println("I'm an Int")
+		default:
+			fmt.Printf("Don't know type %T\n", t)
+		}
+		
+	}
+	whatAmI(true)
+    	whatAmI(1)
+    	whatAmI("hey")
+
+
+
+
+}
+
